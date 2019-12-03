e96de3ea1ed1620edd58b4f8047d5167e9db5c64
Роберт <zajnitdinov@icloud.com>
2019-12-03 19:51:07 +0300

    .

--- src/components/month-list-tasks/month-list-tasks.js
+++ src/components/month-list-tasks/month-list-tasks.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Calendar} from "antd";
+import {Badge, Calendar} from "antd";
+import './month-list-tasks.css'
+import {connect} from "react-redux";
 
-const MonthListTasks = () => {
+const MonthListTasks = ({data}) => {
+    const dateCellRender = (item) => {
+        const now = new Date(item);
+        const dataByDay = data.filter(el => el.date.getDate() === now.getDate() && el.date.getMonth() === now.getMonth());
+        return (
+            <ul className="events">
+                {dataByDay.map(item => (
+                    <li key={item.id}>
+                        <Badge status='warning' text={item.title} />
+                    </li>
+                ))}
+            </ul>
+        )
+    };
+
     return (
-        <Calendar/>
+        <Calendar dateCellRender={dateCellRender}/>
     );
 };
 
-export default MonthListTasks;
+const mapStateToProps = ({items: {data}}) => {
+    return {data}
+};
+
+export default connect(mapStateToProps)(MonthListTasks);
