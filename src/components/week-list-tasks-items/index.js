47e9e75fab532594451c412227c7d166b8cb2b71
Роберт <zajnitdinov@icloud.com>
2019-12-06 16:01:28 +0300

    .

--- src/components/week-list-tasks-items/index.js
+++
@@ -1,3 +0,0 @@
-import WeekListTasksItems from "./week-list-tasks-items";
-
-export default WeekListTasksItems;
--- src/components/week-task-list/week-task-list.js
+++ src/components/week-task-list/week-task-list.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {DatePicker} from "antd";
 import {connect} from "react-redux";
 import WeekTaskListItem from "../week-task-list-item";
 
 const {WeekPicker} = DatePicker;
 
-const WeekTaskList = (props) => {
-    return (
-        <React.Fragment>
-            <WeekPicker disabled={props.loading} style={{marginLeft: '10px'}}/>
-            <WeekTaskListItem {...props}/>
-        </React.Fragment>
-    );
-};
+class WeekTaskList extends Component {
+    state = {
+        data: this.props.data,
+        loading: this.props.loading
+    };
+
+    handleChange = (date, dateString) => {
+        console.log(date, dateString);
+    };
+
+    render(){
+        return (
+            <React.Fragment>
+                <WeekPicker onChange={this.handleChange} disabled={this.props.loading} style={{marginLeft: '10px'}}/>
+                <WeekTaskListItem {...this.props}/>
+            </React.Fragment>
+        );
+    }
+}
 
 const mapStateToProps = ({items: {data, loading} }) => {
     return {
